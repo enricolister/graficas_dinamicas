49f32cf78c0a68d6d1383065e1987999ab0dc36d
Enrico Lister\PonyZero <e.lister@ponyzero.com>
2019-03-13 16:08:59 +0100

    Installed required modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'a4';
-  usuario: string = '';
+  title = 'Graficas dinamicas';
 }
